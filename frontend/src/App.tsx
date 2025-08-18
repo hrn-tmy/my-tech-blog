1ad6e6f2d45d61e63c5f61e09ac8335d09714ee0
hrn-tmy <mj0829mj0625@icloud.com>
2025-08-18 22:41:40 +0900

    add: スピナーといいね合計の追加
    - 「いいね数」をハートのアイコンに変更するもの本コミットに追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { ClipLoader } from "react-spinners";
+import { CiHeart } from "react-icons/ci";
 
 interface Data {
   title: string;
   path: string;
-  liked_count: number
+  liked_count: number;
   published_at: string;
 }
 
 function App() {
   const [articles, setArticles] = useState<Data[]>([]);
   const [count, setCount] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [likeSum, setLikeSum] = useState<number>(0);
   const backendURL = import.meta.env.VITE_BACKEND_URL;
 
   useEffect(() => {
@@ -19,27 +23,40 @@ function App() {
       .then((res) => {
         setArticles(res.data.articles);
         setCount(res.data.articles.length);
+        const totalLikes = res.data.articles.reduce(
+          (sum: number, article: Data) => sum + article.liked_count,
+          -1
+        );
+        setLikeSum(totalLikes);
+        setIsLoading(true);
       })
       .catch((err) => {
         console.error(err);
       });
   }, []);
 
-  return (
+  return !isLoading ? (
+    <div className="flex items-center justify-center h-screen w-screen bg-gray-50">
+      <ClipLoader size={100} />
+    </div>
+  ) : (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">記事一覧 ({count}記事)</h1>
+      <h1 className="text-2xl font-bold mb-4">
+        {" "}
+        記事一覧 ({count}記事 / いいね合計 {likeSum})
+      </h1>
       <div className="overflow-x-auto">
         <table className="min-w-full table-auto border-collapse border border-gray-300">
           <thead>
             <tr className="bg-gray-100">
-              <th className="border border-gray-300 px-4 py-2 text-left">
+              <th className="border border-gray-300 px-2 py-2 text-left">
                 No.
               </th>
               <th className="border border-gray-300 px-4 py-2 text-left">
                 タイトル
               </th>
-              <th className="border border-gray-300 px-4 py-2 text-left">
-                いいね数
+              <th className="border border-gray-300 px-3 py-2 text-left">
+                <CiHeart />
               </th>
               <th className="border border-gray-300 px-4 py-2 text-left">
                 公開日
@@ -55,7 +72,7 @@ function App() {
                 <td className="border border-gray-300 px-4 py-2">
                   <a
                     href={`https://zenn.dev/${article.path}`}
-                    className="text-black hover:underline hover:text-blue-600"
+                    className="text-black underline hover:text-blue-600"
                   >
                     {article.title}
                   </a>
